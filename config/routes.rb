a322f3ec31ee192d192c0ce8977040a184f26e7f
EC2 Default User <ec2-user@ip-172-31-84-239.ec2.internal>
2022-01-29 11:35:26 +0000

    shitumon

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  
+  devise_for :admin
   devise_for :customers
+  
+  namespace :admin do
+    resources :items
+  end
+  
+  namespace :public do
+    resources :items, only: [:index, :show]
+  
+  end
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: "homes#top"
